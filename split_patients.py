6e3dc244721fac9d9fb974fb0b8ea2e31752643a
cottonyjoe <cottonyjoe@gmail.com>
2021-07-01 20:27:16 +0200

    A bit of PEP8 and start distance calculation function

--- split_patients.py
+++ split_patients.py
@@ -2,6 +2,7 @@
 # -*- coding: utf8 -*-
 
 import json
+import math
 
 # This script will read a list of patients from a JSON file and split them into
 # groups according to their nearest vaccination center and sorted by age.
@@ -9,6 +10,7 @@ import json
 # Global variables
 input_file = 'People.txt'
 center_list = 'centerList.txt'
+
 
 # Read data from file
 # @param string File name; optional, default is input_file global var value
@@ -24,16 +26,30 @@ def get_data(file=input_file):
     f.close()
     return data
 
-# Sorts a list of dict objects according to an optional key; this can be reversed.
+
+# Sorts a list of dict objects according to an optional key; this can be
+# reversed.
 # @param list A list of dict objects to sort.
 # @param string The key to sort on; optional, default value is 'Age'.
-# @param boolean Set if sort must be reversed or not; optional, default is True.
+# @param boolean Sets if sort must be reversed or not; optional, default is
+#                True.
 # @return list Sorted list of dict objects
 def sort_by_key(data, sorting_key='Age', is_reversed=True):
-    return sorted(data, key=lambda k: k[sorting_key], reverse=is_reversed) 
+    return sorted(data, key=lambda k: k[sorting_key], reverse=is_reversed)
 
+
+def computation_formula(lat1, long1, lat2, long2):
+    res = 2 * math.asin(math.sqrt(math.pow(, 2)))
+    return
+
+
+# Gets the closest vaccination center from a customer
+# @param dict Customer object with Name, Age, Latitude and Longitude data.
+# @param dict Vaccination centers list of dict with Name, Latitude and
+#             Longitude data.
 def get_closest_center(customer, center_data):
     pass
+
 
 # Main function
 def main():
--- utest_split.py
+++ utest_split.py
@@ -5,12 +5,13 @@ import unittest
 import split_patients as sp
 import json
 
+
 class TestStringMethods(unittest.TestCase):
 
     people = []
-    people.append({"Name": "Neoma Leong","Age": 56,"Latitude": "54.88775482346","Longitude": "-8.167787110571897" })
-    people.append({"Name": "Ezequiel Hepfer","Age": 69,"Latitude": "54.04308476487112","Longitude": "-7.63487375893224" })
-    people.append({"Name": "Lorri Currey","Age": 25,"Latitude": "54.13644761914938","Longitude": "-9.48968906074299" })
+    people.append({"Name": "Neoma Leong", "Age": 56, "Latitude": "54.88775482346", "Longitude": "-8.167787110571897"})
+    people.append({"Name": "Ezequiel Hepfer", "Age": 69, "Latitude": "54.04308476487112", "Longitude": "-7.63487375893224"})
+    people.append({"Name": "Lorri Currey", "Age": 25, "Latitude": "54.13644761914938", "Longitude": "-9.48968906074299"})
 
     # Tests data is read from JSON file
     def test_get_data(self):
@@ -23,9 +24,9 @@ class TestStringMethods(unittest.TestCase):
     def test_sort_by_age_default(self):
         res = sp.sort_by_key(self.people)
         sorted_people = []
-        sorted_people.append({"Name": "Ezequiel Hepfer","Age": 69,"Latitude": "54.04308476487112","Longitude": "-7.63487375893224" })
-        sorted_people.append({"Name": "Neoma Leong","Age": 56,"Latitude": "54.88775482346","Longitude": "-8.167787110571897" })
-        sorted_people.append({"Name": "Lorri Currey","Age": 25,"Latitude": "54.13644761914938","Longitude": "-9.48968906074299" })
+        sorted_people.append({"Name": "Ezequiel Hepfer", "Age": 69, "Latitude": "54.04308476487112", "Longitude": "-7.63487375893224"})
+        sorted_people.append({"Name": "Neoma Leong", "Age": 56, "Latitude": "54.88775482346", "Longitude": "-8.167787110571897"})
+        sorted_people.append({"Name": "Lorri Currey", "Age": 25, "Latitude": "54.13644761914938", "Longitude": "-9.48968906074299"})
         self.assertEqual(res, sorted_people)
         self.assertNotEqual(res, self.people)
 
@@ -33,9 +34,9 @@ class TestStringMethods(unittest.TestCase):
     def test_sort_by_age_increasing(self):
         res = sp.sort_by_key(self.people, is_reversed=False)
         sorted_people = []
-        sorted_people.append({"Name": "Lorri Currey","Age": 25,"Latitude": "54.13644761914938","Longitude": "-9.48968906074299" })
-        sorted_people.append({"Name": "Neoma Leong","Age": 56,"Latitude": "54.88775482346","Longitude": "-8.167787110571897" })
-        sorted_people.append({"Name": "Ezequiel Hepfer","Age": 69,"Latitude": "54.04308476487112","Longitude": "-7.63487375893224" })
+        sorted_people.append({"Name": "Lorri Currey", "Age": 25, "Latitude": "54.13644761914938", "Longitude": "-9.48968906074299"})
+        sorted_people.append({"Name": "Neoma Leong", "Age": 56, "Latitude": "54.88775482346", "Longitude": "-8.167787110571897"})
+        sorted_people.append({"Name": "Ezequiel Hepfer", "Age": 69, "Latitude": "54.04308476487112", "Longitude": "-7.63487375893224"})
         self.assertEqual(res, sorted_people)
         self.assertNotEqual(res, self.people)
 
@@ -43,12 +44,11 @@ class TestStringMethods(unittest.TestCase):
     def test_sort_by_name(self):
         res = sp.sort_by_key(self.people, sorting_key='Name')
         sorted_people = []
-        sorted_people.append({"Name": "Neoma Leong","Age": 56,"Latitude": "54.88775482346","Longitude": "-8.167787110571897" })
-        sorted_people.append({"Name": "Lorri Currey","Age": 25,"Latitude": "54.13644761914938","Longitude": "-9.48968906074299" })
-        sorted_people.append({"Name": "Ezequiel Hepfer","Age": 69,"Latitude": "54.04308476487112","Longitude": "-7.63487375893224" })
+        sorted_people.append({"Name": "Neoma Leong", "Age": 56, "Latitude": "54.88775482346", "Longitude": "-8.167787110571897"})
+        sorted_people.append({"Name": "Lorri Currey", "Age": 25, "Latitude": "54.13644761914938", "Longitude": "-9.48968906074299"})
+        sorted_people.append({"Name": "Ezequiel Hepfer", "Age": 69, "Latitude": "54.04308476487112", "Longitude": "-7.63487375893224"})
         self.assertEqual(res, sorted_people)
         self.assertNotEqual(res, self.people)
-
 
     # Tests distance calculation between 2 GPS coordinates
     def test_distance_calculation(self):
